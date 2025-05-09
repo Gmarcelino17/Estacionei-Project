95ee658e84af49e8e272ebc6c75ab839510ad1f8
Gmarcelino17 <gmarcelino1709@gmail.com>
2025-05-08 21:25:23 -0300

    Update painel.php
    adicionei o bootstrap ao painel e coloquei a funcao login no arquivo funcaoUsuario

--- php/funcoes.php
+++ php/funcoes.php
@@ -1,3 +1,13 @@
-<?php
-    include("funcaoLogin.php");
+<?PHP
+
+include("funcaoCategoria.php");
+include("funcaoMenu.php");
+include("funcaoProduto.php");
+include("funcaoTipoUsuario.php");
+include("funcaoUsuario.php");
+include("funcaoFornecedor.php");
+    
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
 ?>
